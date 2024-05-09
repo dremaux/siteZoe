7b4a26073c7edc3c5d92bacf68646262ac373301
dremaux <ldremaux@btslivh.eu>
2024-05-09 17:19:23 +0200

    save angular matériel mat-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
